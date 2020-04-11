cf19d3f4dd95e76b7c53415ec18fd0c87c0597a1
Daniela Flores <diflores@uc.cl>
2020-04-11 19:39:30 -0400

    add array of points

--- src/render.js
+++ src/render.js
@@ -1,43 +1,45 @@
 const paper = require("paper");
 
 const COLORS = {
-    players: ["#1e90ff", "#9f0000"],
+  players: ["#1e90ff", "#9f0000"],
 };
 
 const EVENTS = {
-    COLLISION: event => new CustomEvent("collision", { detail: event }),
+  COLLISION: event => new CustomEvent("collision", { detail: event }),
 };
 
 const player = (player, index) => {
-    const { x, y } = player;
-
-    const p = new paper.Point(x, y);
-    const path = new paper.Path();
-    path.strokeColor = COLORS.players[index];
-    path.strokeWidth = 20;
-    path.add(new paper.Point(p));
-    path.add(new paper.Point(p.x + 10, p.y + 10));
-    path.smooth()
-    return path;
+  const { x, y } = player;
+  
+  const p = new paper.Point(x, y);
+  const path = new paper.Path();
+  path.strokeColor = COLORS.players[index];
+  path.strokeWidth = 20;
+  path.add(new paper.Point(p.x + 10, p.y + 10));
+  [...Array(10)].forEach((_, index) => {
+    path.add(new paper.Point(p.x + 10 * index, p.y + 10 * index));
+  });
+  path.smooth()
+  return path;
 };
 
 const update = state => {
-    paper.project.clear();
-    const _players = state.players.map(player);
-
-    if (_players[0].intersects(_players[1]))
-        document.dispatchEvent(EVENTS.COLLISION({ player: 0 }));
-
-    paper.view.draw();
+  paper.project.clear();
+  const _players = state.players.map(player);
+  console.log(state.players);
+  if (_players[0].intersects(_players[1]))
+  document.dispatchEvent(EVENTS.COLLISION({ player: 0 }));
+  
+  paper.view.draw();
 };
 
 const gameSetup = () => {
-    const canvas = document.getElementById("canvas");
-    paper.setup(canvas);
+  const canvas = document.getElementById("canvas");
+  paper.setup(canvas);
 };
 
 const renderGame = state => {
-    update(state);
+  update(state);
 };
 
 export { renderGame, gameSetup };
