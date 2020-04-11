a436e449b01878ad51a8c42864221e8600fcfa02
Daniela Flores <diflores@uc.cl>
2020-04-10 20:35:58 -0400

    Replace circles with paths

--- src/render.js
+++ src/render.js
@@ -1,39 +1,43 @@
-const paper = require('paper');
+const paper = require("paper");
 
 const COLORS = {
-  players: ['#1e90ff', '#9f0000'],
-}
-
-const EVENTS = {
-  COLLISION: (event) => new CustomEvent('collision', { detail: event }),
-}
-
-const player = (player, index) => {
-  const { x, y } = player;
-
-  const p = new paper.Point(x, y);
-  const path = new paper.Path.Circle(p, 20);
-  path.fillColor = COLORS.players[index];
-
-  return path;
+    players: ["#1e90ff", "#9f0000"],
 };
 
-const update = (state) => {
-  paper.project.clear();
-  const _players = state.players.map(player);
+const EVENTS = {
+    COLLISION: event => new CustomEvent("collision", { detail: event }),
+};
 
-  if (_players[0].intersects(_players[1])) document.dispatchEvent(EVENTS.COLLISION({ player: 0 }));
+const player = (player, index) => {
+    const { x, y } = player;
 
-  paper.view.draw();
-}
+    const p = new paper.Point(x, y);
+    const path = new paper.Path();
+    path.strokeColor = COLORS.players[index];
+    path.strokeWidth = 20;
+    path.add(new paper.Point(p));
+    path.add(new paper.Point(p.x + 10, p.y + 10));
+    path.smooth()
+    return path;
+};
+
+const update = state => {
+    paper.project.clear();
+    const _players = state.players.map(player);
+
+    if (_players[0].intersects(_players[1]))
+        document.dispatchEvent(EVENTS.COLLISION({ player: 0 }));
+
+    paper.view.draw();
+};
 
 const gameSetup = () => {
-  const canvas = document.getElementById('canvas');
-  paper.setup(canvas);
-}
+    const canvas = document.getElementById("canvas");
+    paper.setup(canvas);
+};
 
-const renderGame = (state) => {
-  update(state);
-}
+const renderGame = state => {
+    update(state);
+};
 
-export { renderGame, gameSetup }
+export { renderGame, gameSetup };
